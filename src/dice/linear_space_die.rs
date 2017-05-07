bc506befbae8aaab48af67601cfa35e625d7aeb8
Ryan Brewster <ryanpbrewster@gmail.com>
2017-05-06 19:05:44 -0700

    Finish implementation of linear space die

--- src/dice/linear_space_die.rs
+++ src/dice/linear_space_die.rs
@@ -1,4 +1,6 @@
 use dice::Die;
+use std::iter::Sum;
+use rand;
 use rand::Rng;
 
 struct LinearSpaceDie {
@@ -7,6 +9,7 @@ struct LinearSpaceDie {
 
 impl LinearSpaceDie {
     pub fn from_probabilities(ps: Vec<f64>) -> LinearSpaceDie {
+        assert!(ps.iter().sum::<f64>() == 1f64);
         LinearSpaceDie { probabilities: ps }
     }
 }
@@ -16,7 +19,29 @@ impl Die for LinearSpaceDie {
         self.probabilities.as_slice()
     }
 
-    fn roll(&self, rng: &mut Rng) -> u32 {
-        unimplemented!()
+    fn roll(&self, rng: &mut Rng) -> usize {
+        let x = rng.next_f64();
+
+        let mut acc = 0f64;
+        for (i, &p) in self.probabilities.iter().enumerate() {
+            acc += p;
+            if acc >= x {
+                return i;
+            }
+        }
+        self.probabilities.len()
     }
+}
+
+#[test]
+#[should_panic]
+fn test_make() {
+    LinearSpaceDie::from_probabilities(vec![0.25, 0.25, 0.25]);
+}
+
+#[test]
+fn test_roll() {
+    let mut rng = rand::thread_rng();
+    let die = LinearSpaceDie::from_probabilities(vec![0.25, 0.25, 0.5]);
+    print!("{:?}\n", die.histogram(&mut rng, 1000));
 }
--- src/dice/mod.rs
+++ src/dice/mod.rs
@@ -2,7 +2,16 @@ use rand::Rng;
 
 pub trait Die {
     fn probabilities(&self) -> &[f64];
-    fn roll(&self, rng: &mut Rng) -> u32;
+    fn roll(&self, rng: &mut Rng) -> usize;
+
+    fn histogram(&self, rng: &mut Rng, n: usize) -> Vec<u32> {
+        let mut hist = vec![0; self.probabilities().len()];
+        for _ in 1..n {
+            let r = self.roll(rng);
+            hist[r] = hist[r] + 1;
+        }
+        hist
+    }
 }
 
 pub mod linear_space_die;
